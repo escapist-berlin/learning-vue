b06a8f5daf57e45d31e88cd6a166e3765430c171
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-06-07 08:17:42 +0200

    Finishing Touches

--- quiz/src/App.vue
+++ quiz/src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="ctr">
-    <questions
-      v-if="questionsAnswered < questions.length"
-      :questions="questions"
-      :questionsAnswered="questionsAnswered"
-      @question-answered="questionAnswered"
+    <transition name="fade" mode="out-in">
+      <questions
+        v-if="questionsAnswered < questions.length"
+        :questions="questions"
+        :questionsAnswered="questionsAnswered"
+        @question-answered="questionAnswered"
 
-    />
-    <result v-else />
-    <button type="button" class="reset-btn">Reset</button>
+      />
+      <result
+        v-else
+        :results="results"
+        :totalCorrect="totalCorrect"
+      />
+    </transition>
+    <button
+      type="button"
+      class="reset-btn"
+      @click.prevent="reset"
+      v-if="questionsAnswered === questions.length"
+    >
+      Reset
+    </button>
   </div>
 </template>
 
@@ -110,11 +123,11 @@ export default {
       }
 
       this.questionsAnswered++;
-    }
+    },
+    reset() {
+      this.questionsAnswered = 0;
+      this.totalCorrect = 0;
+    },
   },
 }
 </script>
-
-<style>
-
-</style>
