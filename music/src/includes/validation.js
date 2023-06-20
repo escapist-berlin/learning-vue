2f0794f95742dadc5eaf219afb54c50157c3c4ce
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-06-20 09:57:25 +0200

    Validating Numbers

--- music/src/includes/validation.js
+++ music/src/includes/validation.js
@@ -6,6 +6,7 @@ import {
 } from "vee-validate";
 import {
   required, min, max, alpha_spaces as alphaSpaces, email,
+  min_value as minVal, max_value as maxVal,
 } from "@vee-validate/rules";
 
 export default {
@@ -19,5 +20,7 @@ export default {
     defineRule('max', max);
     defineRule('alpha_spaces', alphaSpaces);
     defineRule('email', email);
+    defineRule('min_value', minVal);
+    defineRule('max_value', maxVal);
   },
 };
