e6db38d4e8e067ec32d5973af3f8889d203c09ac
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-06-21 10:32:28 +0200

    Validating the Form

--- music/src/components/AppAuth.vue
+++ music/src/components/AppAuth.vue
@@ -90,7 +90,8 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema"
+            @submit="register">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -204,5 +205,10 @@ export default {
       modalVisibility: "isOpen",
     }),
   },
+  methods: {
+    register(values) {
+      console.log(values);
+    }
+  }
 }
 </script>
