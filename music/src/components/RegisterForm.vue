5cb6bc242f608feb3e4a702a60d7814e46255809
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-07-10 11:40:10 +0200

    Logging the user in after Signup

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -114,6 +114,8 @@
 import { auth, usersCollection } from "@/includes/firebase"
 import { createUserWithEmailAndPassword } from "firebase/auth"
 import { addDoc } from "firebase/firestore"
+import { mapWritableState } from "pinia"
+import useUserStore from "@/stores/user"
 
 export default {
   name: "RegisterForm",
@@ -139,6 +141,9 @@ export default {
       reg_alert_variant: "bg-blue-500",
       reg_alert_msg: "Please wait! Your account is being created.",
     };
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"]),
   },
   methods: {
     async register(values) {
@@ -177,6 +182,8 @@ export default {
         this.reg_alert_msg = "An unexpected error occurred. Please try again later.";
         return;
       }
+
+      this.userLoggedIn = true;
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account has been created.";
