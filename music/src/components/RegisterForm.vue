921c29d2a508ba8f33bd099f74fbcf578fc06c42
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-07-18 14:57:44 +0200

    Using Actions

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -110,11 +110,7 @@
 </template>
 
 <script>
-// import firebase from '@/includes/firebase.js'
-import { auth, usersCollection } from "@/includes/firebase"
-import { createUserWithEmailAndPassword } from "firebase/auth"
-import { addDoc } from "firebase/firestore"
-import { mapWritableState } from "pinia"
+import { mapActions } from "pinia"
 import useUserStore from "@/stores/user"
 
 export default {
@@ -142,24 +138,18 @@ export default {
       reg_alert_msg: "Please wait! Your account is being created.",
     };
   },
-  computed: {
-    ...mapWritableState(useUserStore, ["userLoggedIn"]),
-  },
   methods: {
+    ...mapActions(useUserStore, {
+      createUser: "register",
+    }),
     async register(values) {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = "bg-blue-500";
       this.reg_alert_msg = "Please wait! Your account is being created.";
 
-      let userCred = null;
-
       try {
-        userCred = await createUserWithEmailAndPassword(
-          auth,
-          values.email,
-          values.password,
-        );
+        await this.createUser(values);
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = 'bg-red-500';
@@ -167,27 +157,8 @@ export default {
         return;
       }
 
-      try {
-        await addDoc(usersCollection, {
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-          user_type: values.user_type,
-        });
-      } catch (error) {
-        console.log(error.message);
-        this.reg_in_submission = false;
-        this.reg_alert_variant = "bg-red-500";
-        this.reg_alert_msg = "An unexpected error occurred. Please try again later.";
-        return;
-      }
-
-      this.userLoggedIn = true;
-
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account has been created.";
-      console.log(userCred);
     },
   },
 }
--- music/src/stores/user.js
+++ music/src/stores/user.js
@@ -1,7 +1,29 @@
 import { defineStore } from 'pinia';
+import { auth, usersCollection } from "@/includes/firebase"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { addDoc } from "firebase/firestore"
 
 export default defineStore('user', {
   state: () => ({
     userLoggedIn: false,
-  })
+  }),
+  actions: {
+    async register(values) {
+      await createUserWithEmailAndPassword(
+        auth,
+        values.email,
+        values.password,
+      );
+
+      await addDoc(usersCollection, {
+        name: values.name,
+        email: values.email,
+        age: values.age,
+        country: values.country,
+        user_type: values.user_type,
+      });
+
+      this.userLoggedIn = true;
+    }
+  }
 });
