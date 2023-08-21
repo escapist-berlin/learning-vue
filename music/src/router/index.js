f8b02f84bd55c0fa0705b4d124186989c4634917
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-08-21 11:02:43 +0200

    Creating Routes

--- music/src/router/index.js
+++ music/src/router/index.js
@@ -1,6 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from "@/views/HomeView.vue"
+import AboutView from "@/views/AboutView.vue"
 
-const routes = [];
+const routes = [
+  {
+    path: "/", // example.com/
+    component: HomeView,
+  },
+  {
+    path: "/about", // example.com/about
+    component: AboutView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
