9838c66e0099b99ca94a775ffa77e283b388c045
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-08-25 15:56:08 +0200

    Route Meta Fields

--- music/src/router/index.js
+++ music/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import ManageView from '@/views/ManageView.vue'
+import useUserStore from "@/stores/user";
 
 const routes = [
   {
@@ -23,6 +24,9 @@ const routes = [
       console.log('Manage Route Guard');
       next();
     },
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/manage',
@@ -41,9 +45,21 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  console.log("Global Guard");
+  // console.log(to.meta);
 
-  next();
+  if (!to.meta.requiresAuth) {
+    next();
+    return;
+  }
+
+  const store = useUserStore();
+
+  if (store.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
+
 });
 
 export default router;
