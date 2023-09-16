877d25578d22a2278ef52a68db17f4742fe7293b
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-09-16 20:02:07 +0200

    Editing a Song

--- music/src/components/CompositionItem.vue
+++ music/src/components/CompositionItem.vue
@@ -15,6 +15,13 @@
       </button>
     </div>
     <div v-show="showForm">
+      <div
+        class="text-white text-center font-bold p-4"
+        v-if="show_alert"
+        :class="alert_variant"
+        >
+        {{ alert_message }}
+      </div>
       <vee-form
         :validation-schema="schema"
         :initial-values="song"
@@ -43,12 +50,15 @@
         <button
           type="submit"
           class="py-1.5 px-3 rounded text-white bg-green-600"
+          :disabled="in_submission"
         >
           Submit
         </button>
         <button
           type="button"
           class="py-1.5 px-3 rounded text-white bg-gray-600"
+          :disabled="in_submission"
+          @click.prevent="showForm = false"
         >
           Go Back
         </button>
@@ -59,29 +69,64 @@
 
 <script>
 import { ErrorMessage } from 'vee-validate';
+import { db } from "@/includes/firebase";
+import { doc, updateDoc } from "firebase/firestore";
 
 export default {
-    name: 'CompositionItem',
-    components: { ErrorMessage },
-    props: {
-        song: {
-            type: Object,
-            required: true,
-        },
+  name: 'CompositionItem',
+  components: { ErrorMessage },
+  props: {
+    song: {
+      type: Object,
+      required: true,
     },
-    data() {
-        return {
-            showForm: false,
-            schema: {
-                modified_name: 'required',
-                genre: 'alpha_spaces',
-            },
-        };
+    updateSong: {
+      type: Function,
+      required: true,
     },
-    methods: {
-      edit() {
-        console.log('Song edited')
+    index: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      showForm: false,
+      schema: {
+        modified_name: 'required',
+        genre: 'alpha_spaces',
       },
+      in_submission: false,
+      show_alert: false,
+      alert_variant: 'bg-blue-500',
+      alert_message: 'Please wait! Updating song info.',
+    };
+  },
+  methods: {
+    async edit(values) {
+      this.in_submission = true;
+      this.show_alert = true;
+      this.alert_variant = 'bg-blue-500';
+      this.alert_message = 'Please wait! Updating song info.';
+
+      const songRef = doc(db, `songs/${this.song.docID}`);
+
+      try {
+        await updateDoc(songRef, values);
+      } catch(error) {
+        console.log("error while updating", error)
+        this.in_submission = false;
+        this.alert_variant = 'bg-red-500';
+        this.alert_message = 'Something went wrong! Try again later';
+        return;
+      }
+
+      this.updateSong(this.index, values)
+
+      this.in_submission = false;
+      this.alert_variant = 'bg-green-500';
+      this.alert_message = 'Success!';
     },
+  },
 }
 </script>
