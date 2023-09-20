874fb04bd6cbb15e48a26a265d324f8844cd97eb
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-09-20 20:35:50 +0200

    Detecting Query Parameters

--- music/src/views/SongView.vue
+++ music/src/views/SongView.vue
@@ -132,6 +132,11 @@ export default {
       this.$router.push({ name: 'home' });
       return;
     }
+
+    const { sort } = this.$route.query;
+
+    this.sort = sort === '1' || sort === '2' ? sort : '1';
+
     this.song = docSnapshot.data();
     this.getComments();
   },
@@ -177,6 +182,10 @@ export default {
   },
   watch: {
     sort(newVal) {
+      if (newVal === this.$route.query.sort) {
+        return;
+      }
+
       this.$router.push({
         query: {
           sort: newVal,
