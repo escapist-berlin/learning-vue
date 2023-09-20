4a376a190e9f94888b460de77d7d6c4e65d1c459
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-09-20 18:51:20 +0200

    Updating the Comments List

--- music/src/views/SongView.vue
+++ music/src/views/SongView.vue
@@ -61,6 +61,7 @@
         <!-- Sort Comments -->
         <select
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+          v-model="sort"
         >
           <option value="1">Latest</option>
           <option value="2">Oldest</option>
@@ -72,7 +73,7 @@
   <ul class="container mx-auto">
     <li
       class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in comments"
+      v-for="comment in sortedComments"
       :key="comment.docID"
       >
       <!-- Comment Author -->
@@ -109,11 +110,20 @@ export default {
       comment_alert_variant: 'bg-blue-500',
       comment_alert_message: 'Please wait! Your comment is being submitted!',
       comments: [],
+      sort: '1',
     }
   },
   props: ['comment'],
   computed: {
     ...mapState(useUserStore, ["userLoggedIn"]),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if(this.sort === '1') {
+          return new Date(b.datePosted) - new Date(a.datePosted);
+        }
+        return new Date(a.datePosted) - new Date(b.datePosted);
+      });
+    },
   },
   async created() {
     const docSnapshot = await getDoc(doc(songsCollection, this.$route.params.id))
@@ -142,11 +152,11 @@ export default {
 
       await addDoc(commentsCollection, comment);
 
+      this.getComments();
+
       this.comment_in_submission = false;
       this.comment_alert_variant = 'bg-green-500';
       this.comment_alert_message = 'Comment added!';
-
-      console.log("comment", comment)
 
       resetForm();
     },
@@ -163,9 +173,6 @@ export default {
           ...doc.data(),
         })
       });
-
-      console.log("comments", this.comments)
-
     },
   },
 };
