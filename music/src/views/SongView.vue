1f35b856b9fffb5dd83635beb3cf06e0c36f5ad1
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-09-20 20:47:33 +0200

    Updating the Comment Count

--- music/src/views/SongView.vue
+++ music/src/views/SongView.vue
@@ -27,7 +27,7 @@
     >
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments ({{ song.comment_count }})</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -92,7 +92,7 @@
 <script>
 import { ErrorMessage } from 'vee-validate';
 import { songsCollection, commentsCollection, auth } from "@/includes/firebase";
-import { doc, getDoc, addDoc, query, where, getDocs } from "firebase/firestore";
+import { doc, getDoc, addDoc, query, where, getDocs, updateDoc } from "firebase/firestore";
 import { mapState } from "pinia";
 import useUserStore from "@/stores/user";
 
@@ -156,6 +156,10 @@ export default {
       }
 
       await addDoc(commentsCollection, comment);
+
+      this.song.comment_count += 1;
+      await updateDoc(doc(songsCollection, this.$route.params.id), { comment_count: this.song.comment_count });
+
 
       this.getComments();
 
