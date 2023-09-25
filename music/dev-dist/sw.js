a0bad9dc066a240e701e9b0919552eeb6341376b
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-09-25 11:32:36 +0200

    Configuring the Manifest File

--- music/dev-dist/sw.js
+++ music/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.j10rtpma2po"
+    "revision": "0.3nvo8v2ur1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
