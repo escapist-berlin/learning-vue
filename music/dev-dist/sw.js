00f49aa89466a7da53e3bedf6ebd35e8cdaadca7
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-10-09 15:38:24 +0200

    Auto-Registering Global Components

--- music/dev-dist/sw.js
+++ music/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3nvo8v2ur1g"
+    "revision": "0.j2nh0msm77o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
