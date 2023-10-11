1286c7c1684af3578de450cfd4f7893b75fbbe9a
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-10-11 10:23:09 +0200

    Testing the Inner Conten

--- music/src/components/__tests__/about.spec.js
+++ music/src/components/__tests__/about.spec.js
@@ -1,9 +1,17 @@
-import AboutView from '@/views/About.vue';
-import { mount } from '@vue/test-utils';
-import { describe, test, expect, toBe } from "vitest";
+import AboutView from '@/views/AboutView.vue';
+import { mount, shallowMount } from '@vue/test-utils';
+import { describe, test, expect, toContain } from "vitest";
 
 describe('AboutView.vue', () => {
   test('renders inner text', () => {
-    const wrapper = mount(AboutView);
+    const wrapper = shallowMount(AboutView);
+    // const wrapper = mount(AboutView);
+    // const wrapper = mount(AboutView, { shallow: true });
+
+    expect(wrapper.text()).toContain('about');
   })
 });
+
+// shallowMount loads only one level of children in a component
+// shallowMount is much faster and efficient
+// mount loads all the children
