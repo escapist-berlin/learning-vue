930a871ab1a58316e342822dbb4e92277762425b
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-10-16 16:52:47 +0200

    Mocking Promises

--- music/src/components/__tests__/pinia.spec.js
+++ music/src/components/__tests__/pinia.spec.js
@@ -1,6 +1,15 @@
 import { setActivePinia, createPinia } from "pinia";
 import useUserStore from "@/stores/user";
-import { describe, test, expect, toBe, beforeEach } from "vitest";
+import { describe, test, expect, toBe, beforeEach, vi } from "vitest";
+import { auth as firebaseAuth } from "@/includes/firebase"
+
+vi.mock('firebase/auth', () => ({
+  signInWithEmailAndPassword: () => Promise.resolve(),
+}))
+
+vi.mock('@/includes/firebase', () => ({
+  auth: {}
+}))
 
 describe("stores", () => {
   beforeEach(() => {
@@ -11,7 +20,7 @@ describe("stores", () => {
     const store = useUserStore();
 
     expect(store.userLoggedIn).not.toBe(true);
-    await store.authenticate({});
+    await store.authenticate({ email: "test@example.com", password: "password" });
     expect(store.userLoggedIn).toBe(true);
   });
 
