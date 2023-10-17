1fe7553834a7876ca631900919f2d290ae9c084e
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-10-17 11:15:51 +0200

    E2E Sanity Test

--- music/dev-dist/sw.js
+++ music/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fvc6fotbrug"
+    "revision": "0.f53o2h8bs4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
