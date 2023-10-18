c0293365d534c9ef7bc54244de38f50bbb78a594
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-10-18 12:50:37 +0200

    The Reactive Function

--- section-21-3-STARTER/src/App.vue
+++ section-21-3-STARTER/src/App.vue
@@ -2,11 +2,17 @@
   <div>
     <p>{{ num }}</p>
     <button type="button" @click.pevent="increment">Click me</button>
+
+    <p>{{ name }}</p>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive, toRefs } from "vue";
+// ref for Primitive Values / access though .value
+// reacitve for Objects / access directly
+// toRefs allows to make property reactive if we use spread operator
+// spread operator is not available at reactive so we need toRefs
 
 export default {
   name: "App",
@@ -19,9 +25,19 @@ export default {
       num.value++;
     }
 
+    const user = reactive({
+      name: "John",
+      age: 20,
+    });
+
+    setTimeout(() => {
+      user.name = "Luis";
+    }, 3000);
+
     return {
       num,
       increment,
+      ...toRefs(user),
     }
   },
 };
